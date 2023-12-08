fa75aaf8078ea007946690f8679427a44b9471f5
osinkinanastasia@gmail.com <osinkinanastasia@gmail.com>
2023-12-08 16:59:48 +0300

    ааааааааа

---
+++ proga/testing/src/Main.java
@@ -0,0 +1,28 @@
+package Main;
+
+import java.io.ByteArrayInputStream;
+import java.time.Year;
+
+public class Main {
+
+    interface static class Programms {
+        int YearsOfStudying;
+        String naming;
+        int lessons;
+        public void LessonsPerWeek() {
+            System.out.println(lessons);
+        }
+    }
+
+    public static void main(String[] args) {
+        class KT extends Programms {
+
+            Programms.lessons = 13;
+
+            public void LessonsPerWeek
+        }
+
+
+
+    }
+}
